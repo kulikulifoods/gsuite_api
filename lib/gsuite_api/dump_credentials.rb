e46f2961912b97232559dbbf77b98d7f6892cae5
Jordan Moncharmont <jormon@gmail.com>
2018-08-15 15:43:58 -0700

    missing dump_credentials

---
+++ lib/gsuite_api/dump_credentials.rb
@@ -0,0 +1,19 @@
+require "base64"
+
+module GSuiteAPI
+  module DumpCredentials
+    def self.dump
+      if ENV["GOOGLE_CREDENTIALS_BASE64"].present? &&
+          ENV["GOOGLE_APPLICATION_CREDENTIALS"].present?
+        unless File.exist? ENV["GOOGLE_APPLICATION_CREDENTIALS"]
+          credentials = Base64.decode64 ENV["GOOGLE_CREDENTIALS_BASE64"]
+          begin
+            File.write ENV["GOOGLE_APPLICATION_CREDENTIALS"], credentials
+          rescue SystemCallError => e
+            warn "Unable to dump credentials from ENV: #{e}"
+          end
+        end
+      end
+    end
+  end
+end
