b2678d16cecd11009bc341cc9a41c9f9ff1db84d
Jordan Moncharmont <jormon@gmail.com>
2026-05-01 11:23:15 -0700

    support for tables, replace append logic with direct insertion logic

--- lib/gsuite_api/sheets/sheet.rb
+++ lib/gsuite_api/sheets/sheet.rb
@@ -7,11 +7,21 @@ module GSuiteAPI::Sheets
     attr_reader :name, :spreadsheet, :api_object
 
     BATCH_SIZE = 10000
+    ELAPSED_MULTIPLIER = 1.5
 
     def initialize(spreadsheet:, name:, api_object:)
       @spreadsheet = spreadsheet
       @name = name
       @api_object = api_object
+    end
+
+    # ugh -- dirty
+    def refresh!
+      my_sheet_id = @api_object.properties.sheet_id
+      spreadsheet.refresh
+      @api_object = @spreadsheet.api_object.sheets.detect { |s| s.properties.sheet_id == my_sheet_id }
+      @name = api_object.properties.title
+      @a1_table = nil
     end
 
     def row_count
@@ -43,9 +53,7 @@ module GSuiteAPI::Sheets
 
     def upsert_table(values:, value_input_option:, extra_a1_note: nil)
       # touch up the headers
-      service.update_spreadsheet_value \
-        id, range_with_name('1:1'), { values: [values[0]] },
-        value_input_option: value_input_option
+      set range: '1:1', values: [values.first], value_input_option: value_input_option
 
       # replace the data
       replace_table \
@@ -61,22 +69,71 @@ module GSuiteAPI::Sheets
     end
 
     def replace_table(values:, value_input_option:)
-      clear(range: "A2:#{column_name(values.first.count)}#{row_count}")
+      column_count = values.map(&:count).max
+      clear(range: "A2:#{column_name(column_count)}#{row_count}")
 
+      # we don't need this if appending to tables
       row_delta = values.count - (row_count - 1)
       insert_rows(row_delta, start_index: row_count) if row_delta.positive?
       sleep 1 # allow time for row insert to settle
 
-      # write data in batches
-      values.each_slice(BATCH_SIZE).each do |value_slice|
-        elapsed = Benchmark.realtime do
-          service.append_spreadsheet_value \
-            id, range_with_name('A2'), { values: value_slice },
-            value_input_option: value_input_option,
-            insert_data_option: "OVERWRITE"
+      # the tables inside of us may have changed
+      refresh!
+
+      if has_a1_table?
+        # we need to append rows to the table too!
+        current_table_rows = a1_table.range.end_row_index - 1
+
+        if current_table_rows < values.count
+          new_range = a1_table.range.dup
+          new_range.end_row_index = values.count + 1
+          new_range.end_column_index = column_count
+
+          update_table_request = {
+            update_table: {
+              table: {
+                table_id: a1_table.table_id,
+                range: new_range
+              },
+              fields: "range"
+            }
+          }
+
+          update_borders_request = {
+            update_borders: {
+              range: new_range,
+              top: { style: "NONE" },
+              bottom: { style: "NONE" },
+              left: { style: "NONE" },
+              right: { style: "NONE" },
+              inner_horizontal: { style: "NONE" },
+              inner_vertical: { style: "NONE" }
+            }
+          }
+
+          service.batch_update_spreadsheet id, { requests: [update_table_request, update_borders_request] }
+
+          sleep 1 * ELAPSED_MULTIPLIER # allow time for row insert to settle
         end
+      end
+
+      # since we are putting into a table in A1, start at A2
+      start_row = 2
+      # write data in batches, avoid append_cells, update ranges specifically
+      values.each_slice(BATCH_SIZE).with_index do |value_slice, index|
+        end_row = start_row + value_slice.size - 1
+        update_range = "A#{start_row}:#{column_name(column_count)}#{end_row}"
+        elapsed = Benchmark.realtime do
+          service.update_spreadsheet_value \
+            id, range_with_name(update_range), { values: value_slice },
+            value_input_option: value_input_option,
+            include_values_in_response: false
+        end
+
+        # udpate start_row for next batch
+        start_row = start_row + value_slice.size
         # allow time for successive writes to settle
-        sleep elapsed
+        sleep elapsed * ELAPSED_MULTIPLIER
       end
     end
 
@@ -151,12 +208,23 @@ module GSuiteAPI::Sheets
     end
 
     def inspect
-
       format '#<%p id=%p title=%p sheet=%p>', \
              self.class, id, spreadsheet.title, name
     end
 
     protected
+
+    def has_a1_table?
+      a1_table.present?
+    end
+
+    def a1_table
+      @a1_table ||= _a1_table if api_object.respond_to?(:tables)
+    end
+
+    def _a1_table
+      api_object.tables&.detect { |table| table.range.start_column_index == 0 && table.range.start_row_index == 0 }
+    end
 
     def column_name(int)
       name = 'A'
--- lib/gsuite_api/version.rb
+++ lib/gsuite_api/version.rb
@@ -1,3 +1,3 @@
 module GSuiteAPI
-  VERSION = '3.1.2'.freeze
+  VERSION = '3.2.0'.freeze
 end
